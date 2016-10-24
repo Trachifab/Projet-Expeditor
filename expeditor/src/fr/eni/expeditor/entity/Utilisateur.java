9f5fbbe0b698cf523a23642912ebb599ae14487b
Administrateur <Administrateur@42-UC44-15.ad.campus-eni.fr>
2016-10-24 14:02:54 +0200

    init

--- expeditor/src/fr/eni/expeditor/entity/Utilisateur.java
+++ expeditor/src/fr/eni/expeditor/entity/Utilisateur.java
@@ -1,8 +1,42 @@
 package fr.eni.expeditor.entity;
 
+import java.math.BigInteger;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
-public class MonEntity {
+public class Utilisateur {
+
+	@Id
+	private BigInteger identite;
+
+	private String login;
+
+	private String password;
+
+	public BigInteger getIdentite() {
+		return identite;
+	}
+
+	public void setIdentite(BigInteger identite) {
+		this.identite = identite;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
---
+++ expeditor/src/fr/eni/expeditor/service/AbstractService.java
@@ -0,0 +1,10 @@
+package fr.eni.expeditor.service;
+
+import javax.persistence.EntityManager;
+
+public class AbstractService {
+
+	protected EntityManager em;
+	
+	
+}
