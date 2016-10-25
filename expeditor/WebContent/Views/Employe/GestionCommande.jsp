46366cc9ed5c29b5253c86e76a9649b8b85ca7d3
SoulMyke77 <mickael.benard2015@campus-eni.fr>
2016-10-25 14:41:03 +0200

    architecture des vues Initialisation IHM et Servlet

---
+++ expeditor/WebContent/Views/Employe/GestionCommande.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrateur
+  Date: 25/10/2016
+  Time: 14:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Expeditor</title>
+</head>
+<body>
+    L'écran de gestion d'une commande pour l'employé
+</body>
+</html>
---
+++ expeditor/src/fr/eni/expeditor/servlet/GestionArticleServlet.java
@@ -0,0 +1,25 @@
+package fr.eni.expeditor.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Administrateur on 25/10/2016.
+ */
+@WebServlet(name = "GestionArticleServlet")
+public class GestionArticleServlet extends AbstractServlet {
+
+    @Override
+    void action(String action, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    void init(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
