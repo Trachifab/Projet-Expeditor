863687d77e596c490190bbe69c02631cf3e586d4
Joffrey <betourne.joffrey@gmail.com>
2016-10-27 09:07:45 +0200

    ENTITY CLIENT - Modif entité MODULE CSV - En cours (moitié des tâches à finir)

--- expeditor/src/fr/eni/expeditor/entity/Client.java
+++ expeditor/src/fr/eni/expeditor/entity/Client.java
@@ -9,89 +9,108 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
 @Entity
+@NamedQueries(
+        {
+                @NamedQuery(name = "rechercherParIdExterneSinonCreation", query = "select c from Client c where c.idExterne= :idExterne")
+        }
+)
 public class Client {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-	@Column(unique = true)
-	private String idExterne;
+    @Column(unique = true)
+    private String idExterne;
 
-	private String raisonSociale;
+    private String raisonSociale;
 
-	private String adresse1;
+    private String adresse1;
 
-	private String adresse2;
+    private String adresse2;
 
-	private String adresse3;
+    private String adresse3;
 
-	private String cp;
+    private String cp;
 
-	private String ville;
+    private String ville;
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getIdExterne() {
-		return idExterne;
-	}
+    public String getIdExterne() {
+        return idExterne;
+    }
 
-	public void setIdExterne(String idExterne) {
-		this.idExterne = idExterne;
-	}
+    public void setIdExterne(String idExterne) {
+        this.idExterne = idExterne;
+    }
 
-	public String getRaisonSociale() {
-		return raisonSociale;
-	}
+    public String getRaisonSociale() {
+        return raisonSociale;
+    }
 
-	public void setRaisonSociale(String raisonSociale) {
-		this.raisonSociale = raisonSociale;
-	}
+    public void setRaisonSociale(String raisonSociale) {
+        this.raisonSociale = raisonSociale;
+    }
 
-	public String getAdresse1() {
-		return adresse1;
-	}
+    public String getAdresse1() {
+        return adresse1;
+    }
 
-	public void setAdresse1(String adresse1) {
-		this.adresse1 = adresse1;
-	}
+    public void setAdresse1(String adresse1) {
+        this.adresse1 = adresse1;
+    }
 
-	public String getAdresse2() {
-		return adresse2;
-	}
+    public String getAdresse2() {
+        return adresse2;
+    }
 
-	public void setAdresse2(String adresse2) {
-		this.adresse2 = adresse2;
-	}
+    public void setAdresse2(String adresse2) {
+        this.adresse2 = adresse2;
+    }
 
-	public String getAdresse3() {
-		return adresse3;
-	}
+    public String getAdresse3() {
+        return adresse3;
+    }
 
-	public void setAdresse3(String adresse3) {
-		this.adresse3 = adresse3;
-	}
+    public void setAdresse3(String adresse3) {
+        this.adresse3 = adresse3;
+    }
 
-	public String getCp() {
-		return cp;
-	}
+    public String getCp() {
+        return cp;
+    }
 
-	public void setCp(String cp) {
-		this.cp = cp;
-	}
+    public void setCp(String cp) {
+        this.cp = cp;
+    }
 
-	public String getVille() {
-		return ville;
-	}
+    public String getVille() {
+        return ville;
+    }
 
-	public void setVille(String ville) {
-		this.ville = ville;
-	}
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
 
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", idExterne='" + idExterne + '\'' +
+                ", raisonSociale='" + raisonSociale + '\'' +
+                ", adresse1='" + adresse1 + '\'' +
+                ", adresse2='" + adresse2 + '\'' +
+                ", adresse3='" + adresse3 + '\'' +
+                ", cp='" + cp + '\'' +
+                ", ville='" + ville + '\'' +
+                '}';
+    }
 }
--- expeditor/src/fr/eni/expeditor/service/GestionClientBean.java
+++ expeditor/src/fr/eni/expeditor/service/GestionClientBean.java
@@ -1,9 +1,34 @@
 package fr.eni.expeditor.service;
 
+import fr.eni.expeditor.entity.Client;
+
+import javax.ejb.Stateless;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
 /**
  * Created by d1502doreyf on 25/10/2016.
  */
-public class GestionClientBean extends AbstractService{
+@Stateless
+public class GestionClientBean extends AbstractService {
 
+    public Client rechercherParIdentifiant(Integer identite) {
+        return getEntityManager().find(Client.class, identite);
+    }
+
+    public void enregistrerClient(Client client) {
+        getEntityManager().merge(client);
+    }
+
+    public Client rechercherParIdExterneSinonCreation(Client client) {
+        Query q = getEntityManager().createNamedQuery("rechercherParIdExterneSinonCreation");
+        q.setParameter("idExterne", client.getIdExterne());
+        try {
+            client = (Client) q.getSingleResult();
+        } catch (NoResultException nre) {
+            enregistrerClient(client);
+        }
+        return client;
+    }
 
 }
