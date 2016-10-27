da3198f9826f5fe7c7ade86c26074107247a7af1
SoulMyke77 <mickael.benard2015@campus-eni.fr>
2016-10-27 09:12:01 +0200

    suppression erreur de FLorent

--- expeditor/src/fr/eni/expeditor/servlet/GestionArticleServlet.java
+++ expeditor/src/fr/eni/expeditor/servlet/GestionArticleServlet.java
@@ -11,7 +11,6 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.ConstraintViolationException;
 
 import org.jboss.logging.Logger;
 
